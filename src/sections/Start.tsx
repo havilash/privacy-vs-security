7f3499e4c4b184ce1cf69ce1e05d90868499439f
Havilash <havilash2005@gmail.com>
2024-11-20 22:21:14 +0100

    chore: define StartBackground component

--- src/sections/Start.tsx
+++ src/sections/Start.tsx
@@ -1,5 +1,10 @@
-interface Props {}
+import StartBackground from "@/lib/StartBackground";
 
-export default function Start({}: Props) {
-  return <section>Start</section>;
+export default function Start() {
+  return (
+    <section className="h-screen w-full">
+      <StartBackground />
+      <h1>Privacy vs Security</h1>
+    </section>
+  );
 }
