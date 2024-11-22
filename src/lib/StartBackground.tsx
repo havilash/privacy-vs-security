15244c6acb7bec7dd6f858db540acfbe69201373
Havilash <havilash2005@gmail.com>
2024-11-22 22:15:51 +0100

    chore: fix linting

--- src/lib/StartBackground.tsx
+++ src/lib/StartBackground.tsx
@@ -1,35 +1,70 @@
-const Rect = ({ bgColor, tailwindClasses }) => {
-  return (
-    <div
-      className={`absolute border-body-color border-8 ${tailwindClasses}`}
-      style={{
-        backgroundColor: bgColor,
-      }}
-    ></div>
-  );
-};
+interface RectProps {
+  bgColor: string;
+  tailwindClasses: string;
+}
+
+const Rect = ({ bgColor, tailwindClasses }: RectProps) => (
+  <div
+    className={`absolute border-8 border-body-color ${tailwindClasses}`}
+    style={{
+      backgroundColor: bgColor,
+    }}
+  ></div>
+);
 
 export default function StartBackground() {
   const rects = [
-    { bgColor: "#0D2748", tailwindClasses: "left-[30%] top-[16%] w-[53%] h-[64%]" },
-    { bgColor: "#006C7B", tailwindClasses: "left-[47%] top-[74%] w-[17%] h-[15%]" },
-    { bgColor: "#006C7B", tailwindClasses: "left-[19%] top-[-7%] w-[67%] h-[33%]" },
-    { bgColor: "#005773", tailwindClasses: "left-[19%] top-[63%] w-[31%] h-[37%]" },
-    { bgColor: "#003551", tailwindClasses: "left-[-2%] top-[30%] w-[41%] h-[57%]" },
-    { bgColor: "#005069", tailwindClasses: "left-[17%] top-[6%] w-[31%] h-[33%]" },
-    { bgColor: "#005E73", tailwindClasses: "left-[77%] top-[39%] w-[29%] h-[52%]" },
-    { bgColor: "#003D60", tailwindClasses: "left-[59%] top-[55%] w-[29%] h-[40%]" },
-    { bgColor: "#008986", tailwindClasses: "left-[-3%] top-[72%] w-[12%] h-[19%]" },
-    { bgColor: "#007E85", tailwindClasses: "left-[69%] top-[-14%] w-[32%] h-[57%]" },
-    { bgColor: "#007B81", tailwindClasses: "left-[-3%] top-[-7%] w-[29%] h-[49%]" },
+    {
+      bgColor: "#0D2748",
+      tailwindClasses: "left-[30%] top-[16%] w-[53%] h-[64%]",
+    },
+    {
+      bgColor: "#006C7B",
+      tailwindClasses: "left-[47%] top-[74%] w-[17%] h-[15%]",
+    },
+    {
+      bgColor: "#006C7B",
+      tailwindClasses: "left-[19%] top-[-7%] w-[67%] h-[33%]",
+    },
+    {
+      bgColor: "#005773",
+      tailwindClasses: "left-[19%] top-[63%] w-[31%] h-[37%]",
+    },
+    {
+      bgColor: "#003551",
+      tailwindClasses: "left-[-2%] top-[30%] w-[41%] h-[57%]",
+    },
+    {
+      bgColor: "#005069",
+      tailwindClasses: "left-[17%] top-[6%] w-[31%] h-[33%]",
+    },
+    {
+      bgColor: "#005E73",
+      tailwindClasses: "left-[77%] top-[39%] w-[29%] h-[52%]",
+    },
+    {
+      bgColor: "#003D60",
+      tailwindClasses: "left-[59%] top-[55%] w-[29%] h-[40%]",
+    },
+    {
+      bgColor: "#008986",
+      tailwindClasses: "left-[-3%] top-[72%] w-[12%] h-[19%]",
+    },
+    {
+      bgColor: "#007E85",
+      tailwindClasses: "left-[69%] top-[-14%] w-[32%] h-[57%]",
+    },
+    {
+      bgColor: "#007B81",
+      tailwindClasses: "left-[-3%] top-[-7%] w-[29%] h-[49%]",
+    },
   ];
 
   return (
-    <div className="absolute top-0 left-0 w-full h-[110vh]">
+    <div className="absolute left-0 top-0 h-[110vh] w-full">
       {rects.map((rect, index) => (
         <Rect key={index} {...rect} />
       ))}
     </div>
   );
 }
-
--- src/sections/Start.tsx
+++ src/sections/Start.tsx
@@ -2,9 +2,11 @@ import StartBackground from "@/lib/StartBackground";
 
 export default function Start() {
   return (
-    <section className="h-screen w-full flex justify-center items-center">
+    <section className="flex h-screen w-full items-center justify-center">
       <StartBackground />
-      <h1 className="text-black text-[5vw] font-black text-text-color z-50">Privacy vs Security</h1>
+      <h1 className="z-50 text-[5vw] font-black text-text-color">
+        Privacy vs Security
+      </h1>
     </section>
   );
 }
