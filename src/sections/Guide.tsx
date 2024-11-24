7332d2cd1d0d220f1e407c50d45c3412c6a263f3
Havilash <havilash2005@gmail.com>
2024-11-24 16:47:48 +0100

    feat: Guide.tsx

--- src/sections/Guide.tsx
+++ src/sections/Guide.tsx
@@ -1,5 +1,34 @@
 interface Props {}
 
 export default function Guide({}: Props) {
-  return <section>Guide</section>;
+  return (
+    <section className="flex flex-col justify-between gap-16 md:flex-row">
+      <div className="flex flex-col gap-4">
+        <h2>Sicherheit</h2>
+        <p>
+          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
+          excepteur minim irure ex cupidatat proident non aute est. adipisicing
+          laborum proident aute do. ipsum sint magna voluptate nulla non.
+          <br />
+          <br />
+          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
+          excepteur minim irure ex cupidatat proident non aute est. adipisicing
+          laborum proident aute do. ipsum sint magna voluptate nulla non.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2>Privatsphäre</h2>
+        <p>
+          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
+          excepteur minim irure ex cupidatat proident non aute est. adipisicing
+          laborum proident aute do. ipsum sint magna voluptate nulla non.
+          <br />
+          <br />
+          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
+          excepteur minim irure ex cupidatat proident non aute est. adipisicing
+          laborum proident aute do. ipsum sint magna voluptate nulla non.
+        </p>
+      </div>
+    </section>
+  );
 }
--- src/sections/Start.tsx
+++ src/sections/Start.tsx
@@ -4,7 +4,7 @@ export default function Start() {
   return (
     <section className="flex h-screen w-full items-center justify-center">
       <StartBackground />
-      <h1 className="z-50 text-[5vw] text-text-color font-black">
+      <h1 className="z-50 text-[5vw] font-black text-text-color">
         Privacy vs Security
       </h1>
     </section>
