e8bb2dfddeb7d3a8e3bc378636da96ed39745902
blocho135597 <otaka.rokuna@hotmail.com>
2024-12-12 17:07:46 +0100

    Build Fehler behoben (lokal)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+declare module "react-range-slider-input" {
+  import { FC } from "react";
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const ReactRangeSliderInput: FC<any>;
+  export default ReactRangeSliderInput;
+}
