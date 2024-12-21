9e07801a311c1d1b2ac0e494cfac84516ce2a011
blocho135597 <otaka.rokuna@hotmail.com>
2024-12-21 13:52:36 +0100

    Survey Besser Responsive gemacht

--- src/components/SurveyQuestion/SurveyQuestion.tsx
+++ src/components/SurveyQuestion/SurveyQuestion.tsx
@@ -32,9 +32,9 @@ export default function SurveyQuestion({
     onChange(value[1]);
   };
   return (
-    <div className="flex flex-col items-center text-center gap-4 md:flex-row md:text-left md:items-start">
-      <p className="w-full md:w-1/3">{text}</p>
-      <div className="w-full md:w-2/3">
+    <div className="flex flex-col items-center text-center gap-4 lg:flex-row lg:text-left lg:items-start">
+      <p className="w-full lg:w-1/3">{text}</p>
+      <div className="w-full lg:w-2/3">
         <RangeSlider
           className="single-thumb"
           value={singleThumbValue}
