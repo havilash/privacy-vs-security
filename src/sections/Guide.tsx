34b281dbd7bf103faba9e7820a869205826a197b
blocho135597 <otaka.rokuna@hotmail.com>
2024-12-22 15:06:32 +0100

    Update Guide.tsx Text

--- src/sections/Guide.tsx
+++ src/sections/Guide.tsx
@@ -6,22 +6,25 @@ export default function Guide({}: Props) {
       <div className="flex flex-col gap-4">
         <h2>Sicherheit</h2>
         <p>
-          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
-          excepteur minim irure ex cupidatat proident non aute est. adipisicing
-          laborum proident aute do. ipsum sint magna voluptate nulla non.
+          Das Gefühl der Sicherheit ist ein Grundbedürfnis für uns Menschen. Der Staat hat die Verpflichtung, die Sicherheit seiner Bürgerinnen 
+          und Bürger zu gewährleisten. Bei den Chat-Apps ist jetzt die Frage, wie viel Macht und Zugriff der Staat auf diese Apps haben soll.
+          Obwohl bei Chat-Apps Menschen miteinander indirekt und manchmal auch anonym kommunizieren, gibt es viele Vorfälle von Cyberbulling, Sexuelle Belästigung, 
+          Handel mit illegalen Gütern (Drogen, Waffen) bis zu Menschenhandel und Pädophilie. Bei Chat-Apps ist einfach das Problem, dass es keine wirkliche Regeln gibt und 
+          die Polizei oder der Staat hat nicht das Recht bei manchen Vorfälle einzugreiffen, oder sie können nur eingreifen wenn es schon zu spät ist. 
           <br />
           <br />
-          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
-          excepteur minim irure ex cupidatat proident non aute est. adipisicing
-          laborum proident aute do. ipsum sint magna voluptate nulla non.
+          Man ist Pro-Sicherheit, wenn man will das der Staat mehr Recht auf die Nachrichten und Daten der Chat-Apps hat. So können sie verdächtige Chats analysieren 
+          und früh genug handeln. Obwohl dies so klingt wie die NSA bevor Snowden, muss der Staat mit den Users mithilfe Nutzungsbedingungen, Vereinbarungen und Nachrichten klar
+          kommunizieren, was sie mit den Daten machen. Der Staat darf nämlich nicht das Grundbedürfnis der Privatsphäre verletzen um das Grundbedürfnis der Sicherheit zu gewährleisten.
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2>Privatsphäre</h2>
         <p>
-          ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
-          excepteur minim irure ex cupidatat proident non aute est. adipisicing
-          laborum proident aute do. ipsum sint magna voluptate nulla non.
+          Die Privatsphäre ist wie die Sicherheit ein Grundbedürfnis des Menschen. In der Bundesverfassung wird genau geschrieben das jede Person Anspruch auf Achtung 
+          ihres Privat- und Familienlebens, sowie den Schutz vor Missbrauch ihrer persönlichen Daten. Ohne diese Rechte wäre es vermutlich für viele Menschen 
+          unangenehm. Denn ohne die Gewährleistung der Privatsphäre fühlt man sich entblösst und seine persönlichen engen Daten und Geheimnisste könnten von jedem gelesen werden. 
+          
           <br />
           <br />
           ipsum dolore consequat qui laborum pariatur officia veniam est lorem.
